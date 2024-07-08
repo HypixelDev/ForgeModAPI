3a60715b57d9af5ad63d20465504214979a7c85d
Connor Linfoot <connorlinfoot@me.com>
2024-07-08 12:33:10 +0100

    Update to Mod API 1.0

--- src/main/java/net/hypixel/modapi/forge/ForgeModAPI.java
+++ src/main/java/net/hypixel/modapi/forge/ForgeModAPI.java
@@ -26,7 +26,7 @@ import java.util.logging.Logger;
 @Mod(modid = ForgeModAPI.MODID, version = ForgeModAPI.VERSION, clientSideOnly = true, name = "Hypixel Mod API")
 public class ForgeModAPI {
     public static final String MODID = "hypixel_mod_api";
-    public static final String VERSION = "0.5.0";
+    public static final String VERSION = "1.0.0.1";
     private static final Logger LOGGER = Logger.getLogger("HypixelModAPI");
 
     // We store a local reference to the net handler, so it's instantly available from the moment we connect
