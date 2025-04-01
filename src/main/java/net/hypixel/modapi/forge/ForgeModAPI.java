4a13c745a56a5765b4bee2414bebe8c9bf9d54b3
Connor Linfoot <connorlinfoot@me.com>
2025-04-01 14:47:06 +0100

    Implement HypixelModAPIImplementation (#11)
    See https://github.com/HypixelDev/ModAPI/pull/40

--- src/main/java/net/hypixel/modapi/forge/ForgeModAPI.java
+++ src/main/java/net/hypixel/modapi/forge/ForgeModAPI.java
@@ -5,7 +5,10 @@ import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import net.hypixel.modapi.HypixelModAPI;
+import net.hypixel.modapi.HypixelModAPIImplementation;
 import net.hypixel.modapi.packet.HypixelPacket;
+import net.hypixel.modapi.packet.impl.clientbound.ClientboundHelloPacket;
+import net.hypixel.modapi.packet.impl.clientbound.event.ClientboundLocationPacket;
 import net.hypixel.modapi.serializer.PacketSerializer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.network.NetHandlerPlayClient;
@@ -19,23 +22,23 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.network.FMLNetworkEvent;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = ForgeModAPI.MODID, version = ForgeModAPI.VERSION, clientSideOnly = true, name = "Hypixel Mod API")
-public class ForgeModAPI {
+public class ForgeModAPI implements HypixelModAPIImplementation {
     public static final String MODID = "hypixel_mod_api";
     public static final String VERSION = "${version}";
     private static final Logger LOGGER = LogManager.getLogger("HypixelModAPI");
+    private static final boolean DEBUG_MODE = Boolean.getBoolean("net.hypixel.modapi.debug");
 
     // We store a local reference to the net handler, so it's instantly available from the moment we connect
     private NetHandlerPlayClient netHandler;
+    private boolean onHypixel;
 
     @Mod.EventHandler
     public void init(FMLPostInitializationEvent event) {
-        MinecraftForge.EVENT_BUS.register(this);
-        HypixelModAPI.getInstance().setPacketSender(this::sendPacket);
+        HypixelModAPI.getInstance().setModImplementation(this);
     }
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
@@ -47,10 +50,27 @@ public class ForgeModAPI {
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public void onServerDisconnect(FMLNetworkEvent.ClientDisconnectionFromServerEvent event) {
         netHandler = null;
+        onHypixel = false;
     }
 
-    private boolean sendPacket(HypixelPacket packet) {
+    @Override
+    public void onInit() {
+        HypixelModAPI.getInstance().createHandler(ClientboundHelloPacket.class, packet -> onHypixel = true);
+        MinecraftForge.EVENT_BUS.register(this);
+
+        if (DEBUG_MODE) {
+            LOGGER.info("Debug mode is enabled!");
+            registerDebug();
+        }
+    }
+
+    @Override
+    public boolean sendPacket(HypixelPacket packet) {
         if (netHandler == null) {
+            return false;
+        }
+
+        if (!isConnectedToHypixel()) {
             return false;
         }
 
@@ -65,6 +85,11 @@ public class ForgeModAPI {
         packet.write(serializer);
         netHandler.addToSendQueue(new C17PacketCustomPayload(packet.getIdentifier(), buf));
         return true;
+    }
+
+    @Override
+    public boolean isConnectedToHypixel() {
+        return onHypixel;
     }
 
     @ChannelHandler.Sharable
@@ -98,5 +123,13 @@ public class ForgeModAPI {
                 }
             });
         }
+    }
+
+    private static void registerDebug() {
+        // Register events
+        HypixelModAPI.getInstance().subscribeToEventPacket(ClientboundLocationPacket.class);
+
+        HypixelModAPI.getInstance().createHandler(ClientboundLocationPacket.class, packet -> LOGGER.info("Received location packet {}", packet))
+                .onError(error -> LOGGER.error("Received error response for location packet: {}", error));
     }
 }
